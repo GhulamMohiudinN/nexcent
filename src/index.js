e2e591af66df305db99a8569b16544b6a219d2b5
GhulamMohiudinN <mohiudin7174@gmail.com>
2024-03-11 22:20:04 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const link = document.createElement("link");
+link.href =
+  "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap";
+link.rel = "stylesheet";
+
+document.head.appendChild(link);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
