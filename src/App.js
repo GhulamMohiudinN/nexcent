bbd69cef9bb1fbaace1f81d0d4fdc1435b85c0bb
GhulamMohiudinN <mohiudin7174@gmail.com>
2024-03-12 15:50:47 +0500

    comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import Layout from "./layout/Layout";
 
 function App() {
   return (
     <>
-      <div className="bg-red-500 text-red-400 text-[30px]">
-        <p>Lorem ipsum dolor sit amet.</p>
-      </div>
+      <>
+        <Layout />
+      </>
     </>
   );
 }
